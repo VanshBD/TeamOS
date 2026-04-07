8685c78cc7e7f6f6018c79db92b807531164dfcb
Vansh Dobariya <167301367+VanshBD@users.noreply.github.com>
2026-04-07 17:09:33 +0530

    instrument.mjs change and redeploy

--- backend/instrument.mjs
+++ backend/instrument.mjs
@@ -3,12 +3,7 @@ import { ENV } from "./src/config/env.js";
 
 Sentry.init({
   dsn: ENV.SENTRY_DSN,
-  tracesSampleRate: 1.0,
-  profilesSampleRate: 1.0,
+  tracesSampleRate: 0.2,
   environment: ENV.NODE_ENV || "development",
-  includeLocalVariables: true,
-
-  // Setting this option to true will send default PII data to Sentry.
-  // For example, automatic IP address collection on events
   sendDefaultPii: true,
 });
